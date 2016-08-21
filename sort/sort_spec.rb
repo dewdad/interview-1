585e16dec7ea9fe587c5f98a675704836d5a100c
twice-in-a-blue-moon <csjhin@gmail.com>
2016-08-21 18:18:16 -0500

    setup spec test file + gemfile

---
+++ sort/sort_spec.rb
@@ -0,0 +1,15 @@
+require_relative './main'
+
+RSpec.describe SidewalkSort do
+  #context 'end-to-end' do
+  #end
+
+  context '#parse_file' do
+  end
+
+#  context '#sort_file' do
+#  end
+
+#  context '#write_file' do
+#  end
+end
